420f5a515b8b915ea79c239eb89e7d9b7b18bd12
João Lima <joaovictorbernardinodelima@gmail.com>
2025-05-03 11:59:57 -0300

    adiciona exercicios

---
+++ TesteVetor/TesteVetor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace TesteVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] valores = new double[n];
+            for (int i = 0; i < valores.Length; i++)
+            {
+                double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                valores[i] = valor;
+            }
+            double media = 0;
+            for (int i = 0; i < valores.Length;i++)
+            {
+                media += valores[i];
+            }
+            Console.WriteLine(media/valores.Length.ToString(CultureInfo.InvariantCulture);
+
+
+        }
+    }
+}
